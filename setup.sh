a50f3db121effc027387533753dd1bc0ccb054d5
WillChangeThisLater <paulwendt567@gmail.com>
2025-05-03 22:48:30 -0400

    +setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -euox pipefail
+
+#./slink.sh slink.sh
+for file in *.{sh,py}; do
+  if [[ "$file" != "slink.sh" && "$file" != "setup.sh" ]]; then 
+    slink "$file"
+  fi
+done
