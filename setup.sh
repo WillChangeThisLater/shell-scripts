4cd07f26b42a1b92694b8122d380ab4b28db9a95
WillChangeThisLater <paulwendt567@gmail.com>
2025-05-03 22:55:22 -0400

    + setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,10 @@
 
 set -euox pipefail
 
-./slink.sh slink.sh
+# if slink isn't installed, install it
+which slink 2>/dev/null || ./slink.sh slink.sh
+
+# install all the other non-setup, non-slink scripts
 for file in *.{sh,py}; do
   if [[ "$file" != "slink.sh" && "$file" != "setup.sh" ]]; then 
     slink "$file"
